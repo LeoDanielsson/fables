a1fbf2cd06089eda58519680eb9f7629cc56de32
LugoMoll <danielssonleo@gmail.com>
2016-07-08 17:05:24 +0200

    update routing to work in cordova app

--- src/js/router.js
+++ src/js/router.js
@@ -7,6 +7,7 @@ function init(el) {
     page('/overview', context => overviewView(el, context));
     page('/fable/:id', context => fableView(el, context));
 
+    page.base(window.location.pathname);
     page.redirect('/', '/overview');
 
     page({ hashbang: true });
