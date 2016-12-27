c94d7ac3d5051310793003c6589301591606e7bf
LugoMoll <danielssonleo@gmail.com>
2016-12-28 00:35:04 +0100

    new stuff

--- src/js/components/Fable.jsx
+++ src/js/components/Fable.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import Loader from './Loader.jsx';
 
 function Fable({isFetching, fable}) {
-  if(isFetching) {
-    return <Loader/>;
-  }
-  return <div>{ fable.name }</div>
+    if(isFetching) {
+        return <Loader/>;
+    }
+    return (
+      <div>
+          <div className="mdl-grid">
+              <div className="mdl-cell mdl-cell--12-col topbar">
+                  <button
+                      onClick={() => browserHistory.push('/')}
+                      className="back-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+                  >
+                      &lt;&lt; Tillbaka till översikten
+                  </button>
+              </div>
+          </div>
+          <div className="mdl-grid">
+              <div className="mdl-cell mdl-cell--12-col mdl-shadow--2dp fable-description">
+                  <h1>{fable.name}</h1>
+                  <h1>
+                      {fable.artist.name}
+                  </h1>
+                  <h4>Material: {fable.material}</h4>
+              </div>
+          </div>
+      </div>
+    );
 }
 
 function findFable(fables, ownProps) {
--- src/js/router.js
+++ src/js/router.js
@@ -1,8 +1,8 @@
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import React from 'react';
-import store from './store.js'
-import App from './components/App.jsx'
+import store from './store.js';
+import App from './components/App.jsx';
 import Fable from './components/Fable.jsx';
 
 function Root() {
