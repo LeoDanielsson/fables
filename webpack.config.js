3b473020afb298bc55e7e20affc02d4c59f8432a
LugoMoll <danielssonleo@gmail.com>
2017-03-20 10:21:37 +0100

    entry.jsx in lowercase

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ function buildConfig(env) {
     pageViewOnLoad: true
   };
   return {
-    entry: ['./src/js/Entry.jsx'],
+    entry: ['./src/js/entry.jsx'],
     output: {
         path: path.join(__dirname, '/public'),
         filename: 'fables.js',
